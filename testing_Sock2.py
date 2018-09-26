5b9c0273a906849f9da48472e0d6ad19b6e76c26
deepanshu jindal <deepanshujindal.99@gmail.com>
2018-09-26 21:54:41 +0530

    socket testing script

---
+++ testing_Sock2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import thread,time,socket
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.2',5005))
+def send_packet():
+	while(True):
+		time.sleep(1)
+		message = "teststring to "+ UDP_IP + "at " + str(time.time()) 
+		sock.sendto(message,(UDP_IP,UDP_PORT))
+
+def receive_packet():
+	while(True):
+		try:
+			data = sock.recv(1024)
+			if(len(data) != 0):
+				print (str(time.time()) + " received: " + data)
+		except:
+			continue
+
+
+thread.start_new_thread(send_packet,())
+thread.start_new_thread(receive_packet,())
+
+time.sleep(10)
