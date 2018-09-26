36e8ec8ddb55cbda2f8f6e3428f93ba4c01de8f9
Ujjwal Gupta <ujjawalgupta1262@gmail.com>
2018-09-26 15:25:58 +0530

    Debugged and working

--- GBN.py
+++ GBN.py
@@ -4,6 +4,8 @@
 
 import time
 import socket
+import random
+import thread
 from random import randint
 from threading import Lock, Thread
 
@@ -25,7 +27,7 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('',5005))
+sock.bind(('',5005))
 
 class Packet:
 	def __init__(self):
@@ -34,25 +36,26 @@ class Packet:
 		self.host = HOST_ID
 		PACKET_SEQUENCE+=1
 		payload_string = "datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata"
-		payload_length = randint()%MAX_PACKET_LENGTH
+		payload_length = random.randint(0,MAX_PACKET_LENGTH)
 		self.info = payload_string[:payload_length]
 
 class NetworkLayer:
 	def send_packet(self):
+		global NETWORK_LAYER_ENABLED
 		if (NETWORK_LAYER_ENABLED):
 			a = Packet()
 			return a
 
 	def receive_packet(self, packet):
 		f=open("packet_dump"+str(HOST_ID)+".txt","a+")
-		f.write("%f\t%d\n" %(str(time.time(),len(str(packet.seq)) + len(str(packet.host)) + len(packet.info))))
+		f.write(str(time.time()) + " " + str(len(str(packet.seq))) + " " + str(len(str(packet.host))) + " " + str(len(packet.info)))
 		f.close()
 
 #global network layer
 network_layer = NetworkLayer()
 
 class Frame:
-	def __init__(self,frame_kind,seq_nr,ack_nr,info):
+	def __init__(self,seq_nr,ack_nr,info):
 		self.seq = seq_nr
 		self.ack = ack_nr
 		#info is of type Packet
@@ -72,95 +75,120 @@ class DataLinkLayer:
 		self.network_layer_enabled = False
 		self.MAX_SEQ = MAX_SEQ
 		#timer times the last expected ack
-		self.timer = 0
+		self.timer = time.time()
 		self.TIME_FOR_ACK = TIME_FOR_ACK
 		self.frame_available = False
+		self.data_list = []
 
-	def enable_network_layer():
+	def enable_network_layer(self):
+		global NETWORK_LAYER_ENABLED
 		NETWORK_LAYER_ENABLED = True
 
-	def disable_network_layer():
+	def disable_network_layer(self):
+		global NETWORK_LAYER_ENABLED
 		NETWORK_LAYER_ENABLED = False
 
-	def start_timer(frame_nr):
+	def start_timer(self):
 		self.timer = time.time()
 
-	def stop_timer(ack_nr):
+	def stop_timer(self):
 		self.timer = 0
 
-	def time_over():
-		return(time.time() - self.timer) > self.TIME_FOR_ACK
+	def time_over(self):
+		#print time.time() - self.timer, self.TIME_FOR_ACK
+		return (time.time() - self.timer) > self.TIME_FOR_ACK
 
-	def to_physical_layer(frame):
+	def to_physical_layer(self,frame):
 		packet = frame.info
 		packet_str = str(packet.seq) + "_" + str(packet.host) + "_" + str(packet.info)
 		frame_str = str(frame.seq) + "_" +  str(frame.ack)
-		message = packet_str + frame_str 
-		check_sum_string = gen_check_sum(message)
-		message+="_"+check_sum_string
+		message = packet_str + "_" + frame_str 
+		check_sum_string = self.gen_check_sum(message)
+		message += "_" + check_sum_string
 		sock.sendto(message,(UDP_IP,UDP_PORT))
 	
-	def convertStrToFrame(string):
+	def convertStrToFrame(self,string):
 		l = string.split("_")
 		p = Packet()
-		f = Frame()
 		p.seq = int(l[0])
 		p.host = int(l[1])
 		p.info = l[2]
-		f.seq = int(l[3])
-		f.ack = int(l[4])
+		seq = int(l[3])
+		ack = int(l[4])
+		info = p
+		f = Frame(seq,ack,info)
 		f.checksum = l[5]
-		f.info = p
 		return f
 
 	#returns false if frame has not arrived
 	#if frame has arrived, set frame_available to true
-	def from_physical_layer():
+	def from_physical_layer(self):
+			data = self.data_list[0]
+			frame = self.convertStrToFrame(data)
+			return frame
+
+	def run_physical_layer(self):
 		while(True):
 			try:
 				data = sock.recv(MAX_PACKET_LENGTH)
+				print data, "bdfbbbe"
+				self.frame_available = True
+				self.data_list.append(data)
 			except:
 				continue
-			frame = self.convertStrToFrame(data)
-			self.frame_available = True
-			return frame
 
-	def send_data(frame_nr,frame_expected):
-		s = Frame();
-		s.info = self.buffer[frame_nr]
-		s.seq = frame_nr
-		s.ack = (frame_expected + self.MAX_SEQ)%(self.MAX_SEQ + 1)
+	def send_data(self,frame_nr,frame_expected):
+		info = self.buffer[frame_nr]
+		#print frame_nr,info
+		seq = frame_nr
+		ack = (frame_expected + self.MAX_SEQ)%(self.MAX_SEQ + 1)
+		s = Frame(seq,ack,info)
+		packet = s.info
+		frame_str = str(packet.seq) + "_" + str(packet.host) + "_" + str(packet.info) + str(s.seq) + "_" +  str(s.ack)
+		checksum = self.gen_check_sum(frame_str)
+		s.checksum = checksum
 		self.to_physical_layer(s)
-		start_timer()
+		self.start_timer()
 
-	def to_network_layer(packet):
+	def to_network_layer(self,packet):
 		network_layer.receive_packet(packet)
 
-	def from_network_layer():
+	def from_network_layer(self):
+		global NETWORK_LAYER_READY
+		global NETWORK_LAYER_ENABLED
+		if(NETWORK_LAYER_READY and NETWORK_LAYER_ENABLED):
+			packet = network_layer.send_packet()
+			return packet
+
+	def run_network_layer(self):
 		while(True):
+			global NETWORK_LAYER_READY
 			if (int(time.time()*1000)%2 == 1):
 				NETWORK_LAYER_READY = True
 			else:
 				NETWORK_LAYER_READY = False
-			if(NETWORK_LAYER_READY and NETWORK_LAYER_ENABLED):
-				packet = network_layer.send_packet()
-				return packet
 
-	def gen_check_sum(frame_string):
+
+	def gen_check_sum(self,frame_string):
 		return str(hash(frame_string)%1024)
 
-	def check_sum(frame):
+	def check_sum(self,frame):
 		frame_string = str(frame.info.seq)+"_"+str(frame.info.host)+"_"+str(frame.info.info)+"_"+str(frame.seq) + "_" + str(frame.ack)
-		if (gen_check_sum(frame_string)==frame.check_sum):
+		if (self.gen_check_sum(frame_string)==frame.checksum):
 			return True
 		else:
 			return False
 
-	def GBN_protocol():
+	def GBN_protocol(self):
 		self.enable_network_layer();
+		thread.start_new_thread(self.run_network_layer,())
+		thread.start_new_thread(self.run_physical_layer,())
 		while(True):
+			global NETWORK_LAYER_READY
+			print NETWORK_LAYER_READY, NETWORK_LAYER_ENABLED, self.frame_available, self.time_over()
 			#set event here
-			if(NETWORK_LAYER_READY):
+			print self.event
+			if(NETWORK_LAYER_READY and NETWORK_LAYER_ENABLED):
 				self.event = "network_layer_ready"
 			elif(self.frame_available):
 				self.event = "frame_arrival"
@@ -170,15 +198,18 @@ class DataLinkLayer:
 			#handling packets from network layer
 			if(self.event == "network_layer_ready"):
 				packet = self.from_network_layer()
+
+				#print self.next_frame_to_send, self.buffer
 				self.buffer[self.next_frame_to_send] = packet
 				self.nbuffered += 1
+				
 				self.send_data(self.next_frame_to_send,self.frame_expected)
-				self.next_frame_to_send += 1
+				self.next_frame_to_send  = (self.next_frame_to_send + 1)%(self.MAX_SEQ + 1)
 
 			#handling frame arrival at physical layer
 			if(self.event == "frame_arrival"):
 				frame = self.from_physical_layer()
-				if(check_sum(frame) == False):
+				if(self.check_sum(frame) == False):
 					continue;
 				#handle data part
 				if(frame.seq == self.frame_expected):
@@ -187,17 +218,17 @@ class DataLinkLayer:
 					self.to_network_layer(packet)
 					self.frame_expected += 1
 				#handle ack part
-				for i in range(ack_expected,r.ack + 1):
+				for i in range(self.ack_expected,frame.ack + 1):
 					self.nbuffered -= 1
-					if(i == ack_expected):
-						stop_timer()
+					if(i == self.ack_expected):
+						self.stop_timer()
 					self.ack_expected += 1
 
 			#handling time_out
 			if(self.event == "timeout"):
 				#start repeating from the frame whose ack was not received
 				self.next_frame_to_send = self.ack_expected
-				for i in range(nbuffered):
+				for i in range(self.nbuffered):
 					self.send_data(self.next_frame_to_send,self.frame_expected)
 					self.next_frame_to_send += 1
 
@@ -207,7 +238,9 @@ class DataLinkLayer:
 				self.disable_network_layer()
 
 
+d = DataLinkLayer(20,10)
 
+d.GBN_protocol()
 
 
 
